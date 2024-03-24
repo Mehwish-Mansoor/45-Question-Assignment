4d25006a6be2aff0c299afc88a2052eabc287e97
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-24 14:01:42 +0500

    Add files via upload
    i have done my q no 19 of Assignment

---
+++ qno19.ts
@@ -0,0 +1,19 @@
+// 19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestList: string[] = ["Mehwish", "Mehreen", "Misbha", "Simi", "Saima", "Asma"];
+
+console.log(`Dear ${guestList[0]}, you are cordially invited to dinner.`);
+console.log(`Dear ${guestList[1]}, you are cordially invited to dinner.`);
+console.log(`Dear ${guestList[2]}, you are cordially invited to dinner.`);
+console.log(`Dear ${guestList[3]}, you are cordially invited to dinner.`);
+console.log(`Dear ${guestList[4]}, you are cordially invited to dinner.`);
+console.log(`Dear ${guestList[5]}, you are cordially invited to dinner.`);
+
+console.log("We just found out that the new dinner table won't arrive in time for the dinner, and we can only invite two guests.");
+
+while (guestList.length > 2) {
+    let removedGuest = guestList.pop();
+    console.log(`Sorry, ${removedGuest}, we can't invite you to dinner.`);
+}
+
+console.log(`Number of people invited to dinner: ${guestList.length}`);
